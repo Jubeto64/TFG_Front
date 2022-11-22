47fd9e1e8fa34b729e6e9eab6a45255191b7a6ad
Jubeto Oliveira <jubetooliveira@gmail.com>
2022-11-21 22:41:45 -0300

    Salvando o resultado da consulta como CSV

--- scripts/worker.py
+++ scripts/worker.py
@@ -1,4 +1,4 @@
-import pika, json, psycopg2
+import pika, json, psycopg2, sys
 
 conn = psycopg2.connect(
     host="localhost",
@@ -20,12 +20,18 @@ def callback(ch, method, properties, body):
     consulta = json.loads(body.decode())
 
     print(consulta)
+    consulta_sql = consulta['consulta_sql']
 
-    #cur = conn.cursor()
-    #cur.execute('select * from laudo limit 5')
-    #print(cur.fetchall())
+    consulta_sql = consulta_sql.replace('&gt;', '>').replace('&#x27;', '\'').replace('&lt;', '<')
 
-    #cur.close()
+    cur = conn.cursor()
+
+    outputquery = 'copy ({0}) to stdout with csv header'.format(consulta_sql)
+
+    with open('resultsfile.csv', 'w') as f:
+        cur.copy_expert(outputquery, f)
+
+    cur.close()
 
     #print(" [x] Received %r" % body.decode())
     ch.basic_ack(delivery_tag = method.delivery_tag)
